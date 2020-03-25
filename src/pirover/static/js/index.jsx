c321db9017655fd7e795cb6a876e61bfb035eb36
Andrea Conti <contiandrea96@gmail.com>
2020-03-25 12:44:59 +0100

    Add keyboard and touch support

--- src/pirover/static/js/index.jsx
+++ src/pirover/static/js/index.jsx
@@ -22,6 +22,8 @@ function move() {
 
 setInterval(move, 50);
 
+// handle buttons
+
 document.getElementById('leftButton').onmousedown = function() { currMove = 'left' }
 document.getElementById('leftButton').onmouseup = function() { currMove = null }
 document.getElementById('rightButton').onmousedown = function() { currMove = 'right' }
@@ -30,3 +32,41 @@ document.getElementById('forwardButton').onmousedown = function() { currMove = '
 document.getElementById('forwardButton').onmouseup = function() { currMove = null }
 document.getElementById('backwardButton').onmousedown = function() { currMove = 'backward' }
 document.getElementById('backwardButton').onmouseup = function() { currMove = null }
+
+// handle keys
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        currMove = 'left'
+    }
+    else if (event.keyCode == 38) {
+        currMove = 'forward'
+    }
+    else if (event.keyCode == 39) {
+        currMove = 'right'
+    }
+    else if (event.keyCode == 40) {
+        currMove = 'backward'
+    }
+})
+
+
+var keys_ = [37, 38, 39, 40]
+document.addEventListener('keyup', function(event) {
+    if (keys_.includes(event.keyCode)) {
+        currMove = null
+    }
+
+})
+
+// handle touch
+
+document.getElementById('leftButton').addEventListener('touchstart', () => currMove = 'left')
+document.getElementById('leftButton').addEventListener('touchend', () => currMove = null)
+document.getElementById('rightButton').addEventListener('touchstart', () => currMove = 'right')
+document.getElementById('rightButton').addEventListener('touchend', () => currMove = null)
+document.getElementById('forwardButton').addEventListener('touchstart', () => currMove = 'forward')
+document.getElementById('forwardButton').addEventListener('touchend', () => currMove = null)
+document.getElementById('backwardButton').addEventListener('touchstart', () => currMove = 'backward')
+document.getElementById('backwardButton').addEventListener('touchend', () => currMove = null)
+
